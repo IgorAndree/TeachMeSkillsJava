d1fc10eb8d6a5243d55323cb824516496d719165
IgorAndree <mr375mrnobody14021990@gmail.com>
2023-02-08 12:52:29 +0300

    3 lesson: 2p

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,52 @@ public class Main {
         System.out.println("Hello world.Welcome to Java.");
         System.out.println();
         p1();
+        System.out.println();
+        p2();
     }
 
     static void p1() {
-        int num = 0;  // replace this line with the actual number you want to use
 
-        if (num == 0) {
-            System.out.println("This is a single positive number");
+        int num = 375;  // Enter any verification number.
+
+        if (num == 0) {//I decided to highlight "0" as a separate number,+
+            // although I understand that this is an additional line of code.
+            System.out.println("This is a one digit positive number.");
         } else if (num > 0 && num < 10) {
-            System.out.println("This is a single positive number");
+            System.out.println("This is a one digit positive number.");
         } else if (num >= 10 && num < 100) {
-            System.out.println("This is a two-digit positive number");
+            System.out.println("This is a two-digit positive number.");
         } else if (num >= 100 && num < 1000) {
-            System.out.println("This is a three-digit positive number");
+            System.out.println("This is a three-digit positive number.");
         } else if (num >= 0) {
-            System.out.println("This is a four-digit or more positive number");
+            System.out.println("This is a four-digit or more positive number.");
         }
 
         if (num < 0) {
-            if (num > -10) {
-                System.out.println("This is a single negative number");
+            if (num > -10) {//Decided to use a double "if" statement.
+                System.out.println("This is a one digit negative number.");
             } else if (num <= -10 && num > -100) {
-                System.out.println("This is a two-digit negative number");
+                System.out.println("This is a two-digit negative number.");
             } else if (num <= -100 && num > -1000) {
-                System.out.println("This is a three-digit negative number");
+                System.out.println("This is a three-digit negative number.");
             } else {
-                System.out.println("This is a four-digit or more negative number");
+                System.out.println("This is a four-digit or more negative number.");
             }
         }
     }
+    static void p2() {
+
+        int a = 3;
+        int b = 7;
+        int c = 5;
+
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("Triangle exists.");
+        } else {
+            System.out.println("Triangle does not exist.");
+        }
+    }
 }
+
 
 
