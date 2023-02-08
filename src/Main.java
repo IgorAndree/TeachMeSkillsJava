1fd2a43853b69577f9695d6896e7c7894c1a3b94
IgorAndree <mr375mrnobody14021990@gmail.com>
2023-02-08 13:27:27 +0300

    3 lesson: 3p

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ public class Main {
         p1();
         System.out.println();
         p2();
+        System.out.println();
+        p3();
     }
-
     static void p1() {
 
-        int num = 375;  // Enter any verification number.
+        int num = 375; // Enter any verification number.
 
-        if (num == 0) {//I decided to highlight "0" as a separate number,+
-            // although I understand that this is an additional line of code.
+        if (num == 0) { //I decided to highlight "0" as a separate number,+
+                       // although I understand that this is an additional line of code.
             System.out.println("This is a one digit positive number.");
         } else if (num > 0 && num < 10) {
             System.out.println("This is a one digit positive number.");
@@ -38,15 +39,29 @@ public class Main {
     }
     static void p2() {
 
-        int a = 3;
-        int b = 7;
-        int c = 5;
+        int a = 3; // Enter any verification number.
+        int b = 7; // Enter any verification number.
+        int c = 5; // Enter any verification number.
 
         if (a + b > c && a + c > b && b + c > a) {
             System.out.println("Triangle exists.");
         } else {
             System.out.println("Triangle does not exist.");
         }
+    }
+    static void p3() {
+
+        int num = 375; // Enter any verification number.
+        int result;
+
+        if (num > 0) {
+            result = num + 1;
+        } else if (num < 0) {
+            result = num - 2;
+        } else {
+            result = 10; // since in this case the number is only "0".
+        }
+        System.out.println(result);
     }
 }
 
