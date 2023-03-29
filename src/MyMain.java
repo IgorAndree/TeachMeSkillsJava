7fe9387e656edd647f5113fc8d136fd0273550de
IgorAndree <mr375mrnobody14021990@gmail.com>
2023-03-29 12:12:34 +0300

    Домашнее задание 14. Сделал!

--- src/MyMain.java
+++ src/MyMain.java
@@ -4,45 +4,45 @@
 
 
 // Создаем главный класс.
-public class Main {
+public class MyMain {
 
     // Создаем основной метод с точкой входа, пробрасывающий общие ошибки.
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println();
 
-        // Создаем поток Т3.
-        Thread t3 = new Thread(() -> System.out.println("Выполнение потока T3..."));
+        // Создаем поток Т1.
+        Thread t1 = new Thread(() -> System.out.println("Выполнение потока T1..."));
 
         // Создаем поток Т2.
         Thread t2 = new Thread(() -> {
             try {
-                t3.join(); // Дожидаемся завершения потока T3 перед выполнением потока T2.
+                t1.join(); // Дожидаемся завершения потока T1 перед выполнением потока T2.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println("Выполнение потока T2...");
         });
 
-        // Создаем поток Т1.
-        Thread t1 = new Thread(() -> {
+        // Создаем поток Т3.
+        Thread t3 = new Thread(() -> {
             try {
-                t2.join(); // Дожидаемся завершения потока T2 перед выполнением потока T1.
+                t2.join(); // Дожидаемся завершения потока T2 перед выполнением потока T3.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Выполнение потока T1...");
+            System.out.println("Выполнение потока T3...");
         });
 
-        // Запускаем потоки.
-        t1.start();
-        t2.start();
+        // Запускаем потоки в обратном порядке.
         t3.start();
+        t2.start();
+        t1.start();
 
         // Ждем завершения всех потоков.
-        t1.join();
-        t2.join();
         t3.join();
+        t2.join();
+        t1.join();
 
         System.out.println("Все потоки завершили выполнение!");
     }
